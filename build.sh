b5e20ab57c58bc015b9d55148a47b8fc752aa2f8
YESSEO <thespacebirds@gmail.com>
2025-09-08 12:32:36 +0200

    Added building docker compose, and ansi vars

--- build.sh
+++ build.sh
@@ -21,4 +21,39 @@ else
     exit 1
 fi
 
-ansible-playbook -i ansible/inventory.ini ansible/playbooks/sites.yaml --ask-become -vvv
+# Building docker compose
+compose_base="docker/wazuh-docker/multi-node/docker-compose.yml"
+compose_template=$(realpath "./docker/templates/docker-compose.yml")
+if [ -e "$compose_template" ]; then
+    # Generating docker-compose.yml
+    envsubst < "$compose_template" > "$compose_base"
+    # Overwrite the originale docker compose
+    echo "[INFO] Docker Compose generated at ./$compose_base"
+else
+    echo "[ERROR] Make sure the Docker Compose template file exists..."
+    exit 1
+fi
+
+
+file="./ansible/group_vars/all.yml"
+base_dir="$PWD"
+
+if [ ! -f "$file" ]; then
+  echo "[ERROR] File not found: $file" >&2
+  exit 1
+fi
+
+if grep -q '^base_dir:' "$file"; then
+  sed -i "s|^base_dir:.*|base_dir: ${base_dir}|" "$file"
+else
+  echo "base_dir: ${base_dir}" >> "$file"
+fi
+
+echo "[INFO] base_dir set to: $base_dir in $file"
+
+# Run ansible
+ansible-playbook -i ansible/inventory.ini ansible/playbooks/sites.yaml --ask-become
+
+# Wazuh Stack
+ansible-playbook -i ansible/inventory.ini ansible/playbooks/wazuh_stack.yaml --ask-become
+
