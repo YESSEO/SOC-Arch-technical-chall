a4676c0962657281cf8c6ef05bb05f4b03bb3cfb
YESSEO <thespacebirds@gmail.com>
2025-09-13 13:40:53 +0200

    now pipeline fails depending on trivy vulnerablities switch

--- build.sh
+++ build.sh
@@ -21,39 +21,19 @@ else
     exit 1
 fi
 
-# Building docker compose
-compose_base="docker/wazuh-docker/multi-node/docker-compose.yml"
-compose_template=$(realpath "./docker/templates/docker-compose.yml")
-if [ -e "$compose_template" ]; then
-    # Generating docker-compose.yml
-    envsubst < "$compose_template" > "$compose_base"
-    # Overwrite the originale docker compose
-    echo "[INFO] Docker Compose generated at ./$compose_base"
-else
-    echo "[ERROR] Make sure the Docker Compose template file exists..."
-    exit 1
-fi
+# Building ansible var file
+ansible_vars_example=$(realpath "./ansible/group_vars/all_template.yml")
+ansible_vars="$PWD/ansible/group_vars/all.yml"
 
-
-file="./ansible/group_vars/all.yml"
-base_dir="$PWD"
-
-if [ ! -f "$file" ]; then
-  echo "[ERROR] File not found: $file" >&2
+if [ ! -f "$ansible_vars_example" ]; then
+  echo "[ERROR] File not found: $ansible_vars_example" >&2
   exit 1
+else 
+	envsubst < "$ansible_vars_example" > "$ansible_vars"
+        echo "[INFO] Vars file generated at $ansible_vars"
 fi
-
-if grep -q '^base_dir:' "$file"; then
-  sed -i "s|^base_dir:.*|base_dir: ${base_dir}|" "$file"
-else
-  echo "base_dir: ${base_dir}" >> "$file"
-fi
-
-echo "[INFO] base_dir set to: $base_dir in $file"
-
-# Run ansible
-ansible-playbook -i ansible/inventory.ini ansible/playbooks/sites.yaml --ask-become
-
-# Wazuh Stack
-ansible-playbook -i ansible/inventory.ini ansible/playbooks/wazuh_stack.yaml --ask-become
-
+ 
+ansible-playbook -i ansible/inventory.ini --ask-become --ask-vault-pass \
+	ansible/playbooks/trivy.yaml \
+	ansible/playbooks/sites.yaml \
+	ansible/playbooks/wazuh_stack.yaml
